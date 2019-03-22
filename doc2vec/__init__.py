5844b7dd0c2116d2c8b104a91ee318574936cb5a
xuezhengyin210834 <xuezhengyin210834@sohu-inc.com>
2019-03-22 12:03:23 +0800

    docs(README.md):readme.md file

---
+++ doc2vec/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/22 11:34
+# @Author  : xuezhengyin
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ doc2vec/train.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/22 11:35
+# @Author  : xuezhengyin
+# @File    : train.py
+# @Software: PyCharm
